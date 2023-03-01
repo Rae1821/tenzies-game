8fce95e6cfd5dab88a66efa75db41738a9a48341
Rachel Dooley <102691550+Rae1821@users.noreply.github.com>
2023-03-01 13:57:53 -0500

    Added number of rolls and some design changes

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Die(props) {
 
     const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
+        backgroundColor: props.isHeld ? "#6DC8C9" : "white"
     }
 
 
